901f17f14c553fffeeb3fefb852b48559a46403e
CrystalVulpine <crystalvulpine@gmail.com>
2020-08-30 10:49:30 -0400

    Change two ifs to if/else

--- VoxelPlanet/src/mods.cpp
+++ VoxelPlanet/src/mods.cpp
@@ -73,105 +73,6 @@ void mods_processGameArgs(int argc, char *argv[]) {
 	}
 }
 
-#if !defined(_WIN32) && !defined(__MINGW32__)
-
-#include <dlfcn.h>
-
-void loadMods()
-{
-	std::string path("mods/");
-
-    // there are no mods, so don't try to load any
-    if (!fs::is_directory(path)) return;
-
-	mod_testFunc.reserve(5);
-	mod_onRenderTick.reserve(5);
-	mod_onWorldTick.reserve(5);
-	mod_onGameStart.reserve(5);
-	mod_onGameExit.reserve(5);
-	mod_onWorldLoad.reserve(5);
-	mod_onWorldClose.reserve(5);
-	mod_processGameArgs.reserve(5);
-	mod_onGameLoop.reserve(5);
-
-    for(auto& p: fs::recursive_directory_iterator(path))
-    {
-        if(strcmp(p.path().extension().c_str(), ".so") == 0) {
-
-			void* handle;
-
-			if ((handle = dlopen(p.path().string().c_str(), RTLD_LAZY)) != NULL) {
-
-				const char* (*versionGetter)() = (const char* (*)())dlsym(handle, "getApiVersion");
-
-				auto error = dlerror();
-				if (error != NULL) {
-					std::cout << error << '\n';
-					continue;
-				} else {
-					const char* modApiVersion = versionGetter();
-					if (strcmp(modApiVersion, API_VERSION) != 0) {
-						std::cout << "Could not load " << ((const char* (*)())dlsym(handle, "getModName"))() << ". Mod uses API version " << modApiVersion << " but this is " << API_VERSION << ".\n";
-						continue;
-					}
-				}
-
-				std::cout << "Loaded mod " << ((const char* (*)())dlsym(handle, "getModName"))() << ": " << ((const char* (*)())dlsym(handle, "getModDescription"))() << "\n";
-
-				void (*testFunc)() = (void (*)())dlsym(handle, "testFunc");
-				if (dlerror() == NULL) {
-					mod_testFunc.push_back(testFunc);
-				}
-
-				void (*onRenderTick)() = (void (*)())dlsym(handle, "onRenderTick");
-				if (dlerror() == NULL) {
-					mod_onRenderTick.push_back(onRenderTick);
-				}
-
-				void (*onWorldTick)() = (void (*)())dlsym(handle, "onWorldTick");
-				if (dlerror() == NULL) {
-					mod_onWorldTick.push_back(onWorldTick);
-				}
-
-				void (*onGameStart)() = (void (*)())dlsym(handle, "onGameStart");
-				if (dlerror() == NULL) {
-					mod_onGameStart.push_back(onGameStart);
-				}
-
-				void (*onGameExit)() = (void (*)())dlsym(handle, "onGameExit");
-				if (dlerror() == NULL) {
-					mod_onGameExit.push_back(onGameExit);
-				}
-
-				void (*onWorldLoad)() = (void (*)())dlsym(handle, "onWorldLoad");
-				if (dlerror() == NULL) {
-					mod_onWorldLoad.push_back(onWorldLoad);
-				}
-
-				void (*onWorldClose)() = (void (*)())dlsym(handle, "onWorldClose");
-				if (dlerror() == NULL) {
-					mod_onWorldClose.push_back(onWorldClose);
-				}
-
-				void (*onGameLoop)(Clock, Clock) = (void (*)(Clock, Clock))dlsym(handle, "onGameLoop");
-				if (dlerror() == NULL) {
-					mod_onGameLoop.push_back(onGameLoop);
-				}
-
-				void (*processGameArgs)(int, char*[]) = (void (*)(int, char*[]))dlsym(handle, "processGameArgs");
-				if (dlerror() == NULL) {
-					mod_processGameArgs.push_back(processGameArgs);
-				}
-
-			} else {
-
-				std::cout << "Unable to load mod \"" << p.path().filename().string() << "\"\n";
-			}
-        }
-    }
-}
-
-#endif
 
 /*
  * This is the experimental code for loading mods on Windows. It is NOT YET TESTED and probably doesn't work.
@@ -263,6 +164,105 @@ void loadMods()
 
 				void (*processGameArgs)(int, char*[]) = (void (*)(int, char*[]))GetProcAddress(handle, "processGameArgs");
 				if (processGameArgs) {
+					mod_processGameArgs.push_back(processGameArgs);
+				}
+
+			} else {
+
+				std::cout << "Unable to load mod \"" << p.path().filename().string() << "\"\n";
+			}
+        }
+    }
+}
+
+
+#else
+
+#include <dlfcn.h>
+
+void loadMods()
+{
+	std::string path("mods/");
+
+    // there are no mods, so don't try to load any
+    if (!fs::is_directory(path)) return;
+
+	mod_testFunc.reserve(5);
+	mod_onRenderTick.reserve(5);
+	mod_onWorldTick.reserve(5);
+	mod_onGameStart.reserve(5);
+	mod_onGameExit.reserve(5);
+	mod_onWorldLoad.reserve(5);
+	mod_onWorldClose.reserve(5);
+	mod_processGameArgs.reserve(5);
+	mod_onGameLoop.reserve(5);
+
+    for(auto& p: fs::recursive_directory_iterator(path))
+    {
+        if(strcmp(p.path().extension().c_str(), ".so") == 0) {
+
+			void* handle;
+
+			if ((handle = dlopen(p.path().string().c_str(), RTLD_LAZY)) != NULL) {
+
+				const char* (*versionGetter)() = (const char* (*)())dlsym(handle, "getApiVersion");
+
+				auto error = dlerror();
+				if (error != NULL) {
+					std::cout << error << '\n';
+					continue;
+				} else {
+					const char* modApiVersion = versionGetter();
+					if (strcmp(modApiVersion, API_VERSION) != 0) {
+						std::cout << "Could not load " << ((const char* (*)())dlsym(handle, "getModName"))() << ". Mod uses API version " << modApiVersion << " but this is " << API_VERSION << ".\n";
+						continue;
+					}
+				}
+
+				std::cout << "Loaded mod " << ((const char* (*)())dlsym(handle, "getModName"))() << ": " << ((const char* (*)())dlsym(handle, "getModDescription"))() << "\n";
+
+				void (*testFunc)() = (void (*)())dlsym(handle, "testFunc");
+				if (dlerror() == NULL) {
+					mod_testFunc.push_back(testFunc);
+				}
+
+				void (*onRenderTick)() = (void (*)())dlsym(handle, "onRenderTick");
+				if (dlerror() == NULL) {
+					mod_onRenderTick.push_back(onRenderTick);
+				}
+
+				void (*onWorldTick)() = (void (*)())dlsym(handle, "onWorldTick");
+				if (dlerror() == NULL) {
+					mod_onWorldTick.push_back(onWorldTick);
+				}
+
+				void (*onGameStart)() = (void (*)())dlsym(handle, "onGameStart");
+				if (dlerror() == NULL) {
+					mod_onGameStart.push_back(onGameStart);
+				}
+
+				void (*onGameExit)() = (void (*)())dlsym(handle, "onGameExit");
+				if (dlerror() == NULL) {
+					mod_onGameExit.push_back(onGameExit);
+				}
+
+				void (*onWorldLoad)() = (void (*)())dlsym(handle, "onWorldLoad");
+				if (dlerror() == NULL) {
+					mod_onWorldLoad.push_back(onWorldLoad);
+				}
+
+				void (*onWorldClose)() = (void (*)())dlsym(handle, "onWorldClose");
+				if (dlerror() == NULL) {
+					mod_onWorldClose.push_back(onWorldClose);
+				}
+
+				void (*onGameLoop)(Clock, Clock) = (void (*)(Clock, Clock))dlsym(handle, "onGameLoop");
+				if (dlerror() == NULL) {
+					mod_onGameLoop.push_back(onGameLoop);
+				}
+
+				void (*processGameArgs)(int, char*[]) = (void (*)(int, char*[]))dlsym(handle, "processGameArgs");
+				if (dlerror() == NULL) {
 					mod_processGameArgs.push_back(processGameArgs);
 				}
 
